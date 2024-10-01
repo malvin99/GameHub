e43b92b2c77a569e71d5dfe2f63b9902b400f949
Operator26 <41668863+malvin99@users.noreply.github.com>
2024-10-01 14:02:46 +0200

    Updated the Spinner to give a better UX when loading Home Page

--- src/components/SkeletonLoader.tsx
+++ src/components/SkeletonLoader.tsx
@@ -1,17 +1,23 @@
-import { Skeleton, Stack} from "@chakra-ui/react"
-
+import { Flex, Text,Spinner } from "@chakra-ui/react";
 
 function SkeletonLoader () {
-
-    return (
-
-        <Stack>
-            <Skeleton height='20px' />
-            <Skeleton height='20px' />
-            <Skeleton height='20px' />
-        </Stack>
-    )
-
-}
+  return (
+    <Flex
+      height="100vh" // Full height of the viewport
+      width="100%"   // Full width of the viewport
+      alignItems="center" // Center vertically
+      justifyContent="center" // Center horizontally
+    >
+      <Spinner
+                thickness='4px'
+                speed='0.65s'
+                emptyColor='gray.200'
+                color='blue.500'
+                size='xl'
+       />
+      <Text marginLeft={4}>Loading...</Text> {/* Optional loading text */}
+    </Flex>
+  );
+};
 
 export default SkeletonLoader
